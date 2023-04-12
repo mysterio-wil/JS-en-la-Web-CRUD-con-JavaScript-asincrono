e468806f306c48511b2dbd6005b770e1ed071185
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-04-12 09:39:22 -0500

    Creando un cliente

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,18 @@
 const listaClientes = () => 
-    fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({nombre, email, id: uuid.v4()})
+
+  })
+}
 
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
